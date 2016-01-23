4ea458e3734ea5dfc6ded690c039c17312fe2ff2
Aleksey Dobrunov <ctapmex@gmail.com>
2016-01-23 20:31:15 +0500

    add version resource

---
+++ tools/colorer/version.h
@@ -0,0 +1,11 @@
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0"
+
+#define VER_PRODUCTVERSION          VER_FILEVERSION
+#define VER_PRODUCTVERSION_STR      VER_FILEVERSION_STR
+#ifdef WIN64
+  #define CONF " (x64)"
+#else
+  #define CONF ""
+#endif
+#define FILE_DESCRIPTION "Colorer - Syntax Highlighting tools" CONF
