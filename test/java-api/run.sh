ff6e2e18b802dc781fb824e679696acddfc1d31c
Aleksey Dobrunov <ctapmex@gmail.com>
2020-03-01 22:11:15 +0500

    remove old files

--- test/java-api/run.sh
+++
@@ -1,3 +0,0 @@
-export LPATH=../../../../eclipsecolorer/libnative
-
-java -Djava.library.path=$LPATH -cp .:../../../../eclipsecolorer/colorer.jar Test
--- test/vector/gmake.bat
+++
@@ -1,3 +0,0 @@
-gcc -I../../shared/ -g -c deps.cpp
-gcc -I../../shared/ -g -c test-vector.cpp
-gcc -g deps.o test-vector.o -lstdc++ -o test-vector.exe
