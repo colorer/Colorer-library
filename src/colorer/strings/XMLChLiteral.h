94a1d6d3241a9fced326bfeecbe88e0dd2784230
Aleksey Dobrunov <ctapmex@ctapmex.com>
2024-07-24 23:39:07 +0500

    fix build on enabled icu

--- src/colorer/strings/XMLChLiteral.h
+++ src/colorer/strings/XMLChLiteral.h
@@ -1,6 +1,8 @@
 #ifndef COLORER_XMLCHLITERAL_H
 #define COLORER_XMLCHLITERAL_H
 
+#include <xercesc/util/Xerces_autoconf_config.hpp>
+
 // crutch for a case when Xerces has XMLCh be alias for uint16_t instead of char16_t
 // so far, it has only been found on macOS
 
