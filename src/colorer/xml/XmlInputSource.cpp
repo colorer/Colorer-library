7a0cacae2c9428b59b22bb3ad9c365a7fab7bd62
Aleksey Dobrunov <ctapmex@ctapmex.com>
2024-09-30 21:25:59 +0500

    remove xerces-c depends

--- src/colorer/xml/XmlInputSource.cpp
+++ src/colorer/xml/XmlInputSource.cpp
@@ -2,23 +2,9 @@
 
 XmlInputSource::XmlInputSource(const UnicodeString& source_path) : XmlInputSource(source_path, nullptr) {}
 
-XmlInputSource::~XmlInputSource()
-{
-#ifndef COLORER_FEATURE_LIBXML
-  //xml_input_source child of xercesc classes and need to free before xercesc
-  xml_input_source.reset();
-  xercesc::XMLPlatformUtils::Terminate();
-#endif
-}
-
 XmlInputSource::XmlInputSource(const UnicodeString& source_path, const UnicodeString* source_base)
 {
-#ifdef COLORER_FEATURE_LIBXML
   xml_input_source = std::make_unique<LibXmlInputSource>(source_path, source_base);
-#else
-  xercesc::XMLPlatformUtils::Initialize();
-  xml_input_source = XercesXmlInputSource::newInstance(&source_path, source_base);
-#endif
 }
 
 uXmlInputSource XmlInputSource::createRelative(const UnicodeString& relPath) const
@@ -31,13 +17,6 @@ UnicodeString& XmlInputSource::getPath() const
 {
   return xml_input_source->getPath();
 }
-
-#ifndef COLORER_FEATURE_LIBXML
-XercesXmlInputSource* XmlInputSource::getInputSource() const
-{
-  return xml_input_source.get();
-}
-#endif
 
 bool XmlInputSource::isFileURI(const UnicodeString& path, const UnicodeString* base)
 {
