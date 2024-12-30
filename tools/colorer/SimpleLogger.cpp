05efa4f2eee9b7438ce242f47f7b0d2de0d19011
Aleksey Dobrunov <ctapmex@ctapmex.com>
2024-12-30 23:35:37 +0500

    split SimpleLogger into two files; fix warn;

---
+++ tools/colorer/SimpleLogger.cpp
@@ -0,0 +1,77 @@
+#include "SimpleLogger.h"
+
+const std::string_view SimpleLogger::LogLevelStr[] {"off", "error", "warning", "info", "debug", "trace"};
+
+SimpleLogger::SimpleLogger(const std::string_view& filename, const std::string& log_level)
+{
+  current_level = getLogLevel(log_level);
+  log_filename = filename;
+  open_logfile();
+}
+
+SimpleLogger::SimpleLogger(const std::string_view& filename, const Logger::LogLevel log_level)
+{
+  current_level = log_level;
+  log_filename = filename;
+  open_logfile();
+}
+
+SimpleLogger::~SimpleLogger()
+{
+  ofs.close();
+}
+
+void SimpleLogger::open_logfile()
+{
+  if (current_level == LOG_OFF || ofs.is_open()) {
+    return;
+  }
+  ofs.open(log_filename.c_str(), std::ofstream::out | std::ofstream::app);
+  if (!ofs.is_open()) {
+    throw std::runtime_error("Could not open file to write logs: " + log_filename);
+  }
+}
+
+void SimpleLogger::log(Logger::LogLevel level, const char* /*filename_in*/, int /*line_in*/,
+                       const char* /*funcname_in*/, const char* message)
+{
+  if (level > current_level) {
+    return;
+  }
+  std::time_t const t = std::time(nullptr);
+  char mbstr[30];
+  std::strftime(mbstr, sizeof(mbstr), "%FT%T", std::localtime(&t));
+  ofs << "[" << mbstr << "] "
+      << "[" << LogLevelStr[level] << "] ";
+  ofs << message << '\n';
+}
+
+Logger::LogLevel SimpleLogger::getLogLevel(const std::string& log_level)
+{
+  int i = 0;
+  for (auto it : LogLevelStr) {
+    if (log_level == it) {
+      return static_cast<Logger::LogLevel>(i);
+    }
+    i++;
+  }
+  if (log_level == "warn") {
+    return Logger::LOG_WARN;
+  }
+  return Logger::LOG_OFF;
+}
+
+void SimpleLogger::setLogLevel(Logger::LogLevel level)
+{
+  ofs.flush();
+  if (current_level == Logger::LOG_OFF) {
+    current_level = level;
+    open_logfile();
+  }
+  current_level = level;
+}
+
+void SimpleLogger::flush()
+{
+  ofs.flush();
+}
--- tools/colorer/SimpleLogger.h
+++ tools/colorer/SimpleLogger.h
@@ -1,7 +1,6 @@
 #ifndef SIMPLELOGGER_H
 #define SIMPLELOGGER_H
 
-#include <ctime>
 #include <fstream>
 #include <locale>
 #include "colorer/common/Logger.h"
@@ -9,75 +8,23 @@
 class SimpleLogger : public Logger
 {
  public:
-  static constexpr std::string_view LogLevelStr[] {"off", "error", "warning", "info", "debug", "trace"};
+  static const std::string_view LogLevelStr[];
 
-  SimpleLogger(const std::string& filename, const std::string& log_level)
-  {
-    current_level = getLogLevel(log_level);
-    log_filename = filename;
-    open_logfile();
-  }
+  SimpleLogger(const std::string_view& filename, const std::string& log_level);
 
-  SimpleLogger(const std::string& filename, const Logger::LogLevel log_level)
-  {
-    current_level = log_level;
-    log_filename = filename;
-    open_logfile();
-  }
+  SimpleLogger(const std::string_view& filename, Logger::LogLevel log_level);
 
-  ~SimpleLogger() override { ofs.close(); }
+  ~SimpleLogger() override;
 
-  void open_logfile()
-  {
-    if (current_level == LOG_OFF || ofs.is_open()) {
-      return;
-    }
-    ofs.open(log_filename.c_str(), std::ofstream::out | std::ofstream::app);
-    if (!ofs.is_open()) {
-      throw std::runtime_error("Could not open file to write logs: " + log_filename);
-    }
-  }
+  void open_logfile();
 
   void log(Logger::LogLevel level, const char* /*filename_in*/, int /*line_in*/, const char* /*funcname_in*/,
-           const char* message)
-  {
-    if (level > current_level) {
-      return;
-    }
-    std::time_t const t = std::time(nullptr);
-    char mbstr[30];
-    std::strftime(mbstr, sizeof(mbstr), "%FT%T", std::localtime(&t));
-    ofs << "[" << mbstr << "] "
-        << "[" << LogLevelStr[level] << "] ";
-    ofs << message << '\n';
-  }
+           const char* message) override;
 
-  static Logger::LogLevel getLogLevel(const std::string& log_level)
-  {
-    int i = 0;
-    for (auto it : LogLevelStr) {
-      if (log_level == it) {
-        return static_cast<Logger::LogLevel>(i);
-      }
-      i++;
-    }
-    if (log_level == "warn") {
-      return Logger::LOG_WARN;
-    }
-    return Logger::LOG_OFF;
-  }
+  static Logger::LogLevel getLogLevel(const std::string& log_level);
 
-  void setLogLevel(Logger::LogLevel level)
-  {
-    ofs.flush();
-    if (current_level == Logger::LOG_OFF) {
-      current_level = level;
-      open_logfile();
-    }
-    current_level = level;
-  }
-
-  void flush() { ofs.flush(); }
+  void setLogLevel(Logger::LogLevel level);
+  void flush() override;
 
  private:
   std::ofstream ofs;
